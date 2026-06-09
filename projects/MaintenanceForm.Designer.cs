4520d098b1003e4e693cd709ef2b7be18973ea5b
Edgar Camu Jr. <edgarcamujr2000@gmail.com>
2026-06-09 17:16:23 +0800

    setup maintenance module UI and crud logic structure

--- projects/MaintenanceForm.Designer.cs
+++ projects/MaintenanceForm.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace projects
+﻿namespace project
 {
     partial class MaintenanceForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,158 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbTableSelect = new System.Windows.Forms.ComboBox();
+            this.dgvMaintenanceData = new System.Windows.Forms.DataGridView();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblValueName = new System.Windows.Forms.Label();
+            this.txtValueName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenanceData)).BeginInit();
+            this.SuspendLayout();
+             
+            // cmbTableSelect            
+            this.cmbTableSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTableSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTableSelect.FormattingEnabled = true;
+            this.cmbTableSelect.Items.AddRange(new object[] {
+            "Departments",
+            "Positions",
+            "Employment Types",
+            "Requirement Types",
+            "Interview Types",
+            "Assessment Types"});
+            this.cmbTableSelect.Location = new System.Drawing.Point(30, 60);
+            this.cmbTableSelect.Name = "cmbTableSelect";
+            this.cmbTableSelect.Size = new System.Drawing.Size(220, 28);
+            this.cmbTableSelect.TabIndex = 0;
+            this.cmbTableSelect.SelectedIndexChanged += new System.EventHandler(this.cmbTableSelect_SelectedIndexChanged);
+            
+            // dgvMaintenanceData           
+            this.dgvMaintenanceData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaintenanceData.Location = new System.Drawing.Point(280, 60);
+            this.dgvMaintenanceData.Name = "dgvMaintenanceData";
+            this.dgvMaintenanceData.RowHeadersWidth = 51;
+            this.dgvMaintenanceData.Size = new System.Drawing.Size(530, 380);
+            this.dgvMaintenanceData.TabIndex = 1;
+            this.dgvMaintenanceData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMaintenanceData_CellClick);
+            
+            // lblID           
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(30, 110);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(73, 16);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "Record ID:";
+            
+            // txtID           
+            this.txtID.Location = new System.Drawing.Point(30, 135);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(220, 22);
+            this.txtID.TabIndex = 3;
+             
+            // lblValueName           
+            this.lblValueName.AutoSize = true;
+            this.lblValueName.Location = new System.Drawing.Point(30, 180);
+            this.lblValueName.Name = "lblValueName";
+            this.lblValueName.Size = new System.Drawing.Size(121, 16);
+            this.lblValueName.TabIndex = 4;
+            this.lblValueName.Text = "Value Name / Title:";
+            
+            // txtValueName            
+            this.txtValueName.Location = new System.Drawing.Point(30, 205);
+            this.txtValueName.Name = "txtValueName";
+            this.txtValueName.Size = new System.Drawing.Size(220, 22);
+            this.txtValueName.TabIndex = 5;
+            
+            // btnAdd          
+            this.btnAdd.Location = new System.Drawing.Point(30, 260);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 35);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            
+            // btnUpdate
+            this.btnUpdate.Location = new System.Drawing.Point(150, 260);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 35);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            
+            // btnDelete
+            this.btnDelete.Location = new System.Drawing.Point(30, 310);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 35);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            
+            // btnClear
+            this.btnClear.Location = new System.Drawing.Point(150, 310);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 35);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear Fields";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            
+            // lblHeader
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(26, 25);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(250, 20);
+            this.lblHeader.TabIndex = 10;
+            this.lblHeader.Text = "Select Maintenance Category:";
+            
+            // MaintenanceForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MaintenanceForm";
+            this.ClientSize = new System.Drawing.Size(840, 470);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValueName);
+            this.Controls.Add(this.lblValueName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.dgvMaintenanceData);
+            this.Controls.Add(this.cmbTableSelect);
+            this.Name = "MaintenanceForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maintenance Module";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenanceData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbTableSelect;
+        private System.Windows.Forms.DataGridView dgvMaintenanceData;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblValueName;
+        private System.Windows.Forms.TextBox txtValueName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblHeader;
     }
 }
--- projects/MaintenanceForm.cs
+++ projects/MaintenanceForm.cs
@@ -1,20 +1,241 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace projects
+namespace project
 {
     public partial class MaintenanceForm : Form
     {
+        
+        DatabaseConnection db = new DatabaseConnection();
+        
+       
+        string currentTable = ""; 
+        string primaryKeyColumn = "";
+        string valueColumn = "";
+
         public MaintenanceForm()
         {
             InitializeComponent();
+            
+            
+            cmbTableSelect.SelectedIndex = 0; 
+        }
+
+        
+        void SetTableMapping()
+        {
+            if (cmbTableSelect.SelectedItem == null)
+            {
+                return;
+            }
+
+            string selected = cmbTableSelect.SelectedItem.ToString();
+
+           
+            if (selected == "Departments")
+            {
+                currentTable = "departments";
+                primaryKeyColumn = "id";
+                valueColumn = "name";
+            }
+            else if (selected == "Positions")
+            {
+                currentTable = "job_vacancies";
+                primaryKeyColumn = "id";
+                valueColumn = "title";
+            }
+            else if (selected == "Employment Types")
+            {
+                currentTable = "employment_types";
+                primaryKeyColumn = "id";
+                valueColumn = "type_name";
+            }
+            else if (selected == "Requirement Types")
+            {
+                currentTable = "requirement_types";
+                primaryKeyColumn = "id";
+                valueColumn = "name";
+            }
+            else if (selected == "Interview Types")
+            {
+                currentTable = "interview_types";
+                primaryKeyColumn = "id";
+                valueColumn = "type_name";
+            }
+            else if (selected == "Assessment Types")
+            {
+                currentTable = "assessment_types";
+                primaryKeyColumn = "id";
+                valueColumn = "type_name";
+            }
+        }
+
+        
+        public void LoadMaintenanceData()
+        {
+            try
+            {
+                SetTableMapping();
+                
+                if (currentTable == "")
+                {
+                    return;
+                }
+
+               
+                string query = "SELECT " + primaryKeyColumn + " AS ID, " + valueColumn + " AS Name FROM " + currentTable;
+                
+                DataTable dt = db.Query(query);
+                dgvMaintenanceData.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+        private void cmbTableSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadMaintenanceData();
+            ClearFields();
+        }
+
+        
+        private void dgvMaintenanceData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvMaintenanceData.Rows[e.RowIndex];
+                txtID.Text = row.Cells["ID"].Value.ToString();
+                txtValueName.Text = row.Cells["Name"].Value.ToString();
+            }
+        }
+
+        
+
+        // 1. ADD NEW RECORD
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            if (txtValueName.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill up the Value Name textbox first.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                string query = "INSERT INTO " + currentTable + " (" + valueColumn + ") VALUES (@val)";
+                
+                
+                var p1 = ("@val", (object)txtValueName.Text.Trim());
+                int result = db.Execute(query, p1);
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Record has been added successfully!", "Success");
+                    
+                    
+                    Audit.Log("Added reusable value", currentTable, 0, "Value: " + txtValueName.Text.Trim());
+                    
+                    LoadMaintenanceData();
+                    ClearFields();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error on inserting record: " + ex.Message);
+            }
+        }
+
+        // 2. UPDATE EXISTING RECORD
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            if (txtID.Text == "")
+            {
+                MessageBox.Show("Please select a record from the list first.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtValueName.Text.Trim() == "")
+            {
+                MessageBox.Show("Textbox cannot be blank.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                string query = "UPDATE " + currentTable + " SET " + valueColumn + " = @val WHERE " + primaryKeyColumn + " = @id";
+                
+                var p1 = ("@val", (object)txtValueName.Text.Trim());
+                var p2 = ("@id", (object)txtID.Text);
+                int result = db.Execute(query, p1, p2);
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Record has been updated!", "Success");
+                    
+                    Audit.Log("Updated reusable value", currentTable, Convert.ToInt32(txtID.Text), "New Value: " + txtValueName.Text.Trim());
+                    
+                    LoadMaintenanceData();
+                    ClearFields();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error on updating record: " + ex.Message);
+            }
+        }
+
+        // 3. DELETE RECORD
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "")
+            {
+                MessageBox.Show("Please select a record to delete.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            DialogResult response = MessageBox.Show("Are you sure you really want to delete this record?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if (response == DialogResult.Yes)
+            {
+                try
+                {
+                    string query = "DELETE FROM " + currentTable + " WHERE " + primaryKeyColumn + " = @id";
+                    
+                    var p1 = ("@id", (object)txtID.Text);
+                    int result = db.Execute(query, p1);
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Record deleted successfully.", "Success");
+                        
+                        Audit.Log("Deleted reusable value", currentTable, Convert.ToInt32(txtID.Text), "Deleted ID: " + txtID.Text);
+                        
+                        LoadMaintenanceData();
+                        ClearFields();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error on deleting record: " + ex.Message);
+                }
+            }
+        }
+
+       
+        void ClearFields()
+        {
+            txtID.Text = "";
+            txtValueName.Text = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
         }
     }
 }
